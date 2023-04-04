a315eb42159254b33985ec9c1b071ad08638ad82
Stefano <tepo@hotmail.it>
2023-04-04 21:27:55 +0200

    setting up useReducer

--- src/Stories.js
+++ src/Stories.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 import { useGlobalContext } from './context'
+import SearchForm from './SearchForm'
+import Buttons from './Buttons'
 
 const Stories = () => {
-  return <h2>stories component</h2>
+  const {isLoading} = useGlobalContext();
+
+  if (isLoading) {
+    return <div className='loading'></div>
+  }
+
+  return <h2>
+    stories
+  </h2>
 }
 
 export default Stories
